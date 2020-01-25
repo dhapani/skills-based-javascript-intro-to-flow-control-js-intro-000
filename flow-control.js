ca32c5db296d24f8db91877e203df4073c363c08
hilarious-tier-9201 <dhirahapani@hotmail.com>
2020-01-25 02:40:22 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,16 +1,14 @@
 function basicTeenager(age) {
-  if (age === '13-19')
-  console.log(You are a teenager)
+  if (age>=13 && age<=19) {
+  return "You are a teenager!",
 }
-function teenager(age) {
-  if (age < 12 && age > 19)
-console.log(You are not a teenager)
 }
+function teenager(age)
 
+}
 function ageChecker(age) {
 
 }
-
 function ternaryTeenager(age) {
 
 }
